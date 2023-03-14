4062849437d730dd5d8b2bbba5248d19eafa4dff
Lukskyboy <Lukskyboy@gmail.com>
2023-03-14 18:15:13 +0100

    System elektroniczny do oceny pracowników

---
+++ System elektroniczny do oceny pracowników/System elektroniczny do oceny pracowników/Program.cs
@@ -0,0 +1,14 @@
+﻿string Imie = ("Ewa");
+bool kobieta = true;
+var Wiek = 33;
+
+
+if (kobieta == true && Wiek - 4 == 29)
+{ Console.WriteLine("Kobieta poniżej 30 lat"); }
+
+if (Imie == "Ewa" && kobieta == true && Wiek == 33)
+{ Console.WriteLine("Ewa, lat 33"); }
+
+if (Wiek < 18 && kobieta == false)
+{ Console.WriteLine("Niepełnoletni mężczyzna"); }
+
