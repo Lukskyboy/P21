cd72b5fabd51de02f63d5d89b17a18eb0b44bf14
Lukskyboy <Lukskyboy@gmail.com>
2023-03-15 00:49:56 +0100

    ChallengeApp

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿string Imie = "Ewa";
+bool Kobieta = true;
+var Wiek = 33;
+
+{
+    if (Kobieta = true && Wiek < 33)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+
+    else if (Kobieta = true && Imie == "Ewa" && Wiek == 33)
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+    else if (Kobieta == false && Wiek < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+
+    else
+    {
+        Console.WriteLine("Inni");
+    }
+}
