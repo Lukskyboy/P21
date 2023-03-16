fb4d0b9082649f8ef2aea468d0c2dd8cf5203d10
Lukskyboy <Lukskyboy@gmail.com>
2023-03-16 04:04:06 +0100

    Lista + Foreach

---
+++ Zastosowanie Listy z Pętlą Foreach/Zastosowanie Listy z Pętlą Foreach/Program.cs
@@ -0,0 +1,14 @@
+﻿List<string> dayOfWeeks = new List<string>();
+
+dayOfWeeks.Add("Poniedziałek");
+dayOfWeeks.Add("Wtorek");
+dayOfWeeks.Add("Środa");
+dayOfWeeks.Add("Czwartek");
+dayOfWeeks.Add("Piątek");
+dayOfWeeks.Add("Sobota");
+dayOfWeeks.Add("Niedziela");
+
+foreach (var day in dayOfWeeks)
+{
+    Console.WriteLine(day);
+}
