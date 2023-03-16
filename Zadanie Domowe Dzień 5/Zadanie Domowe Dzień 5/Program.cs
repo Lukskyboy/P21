e7becb948a63d678e60d514e934fa4e59b51dee7
Lukskyboy <Lukskyboy@gmail.com>
2023-03-16 04:41:39 +0100

    Dzień 5

---
+++ Zadanie Domowe Dzień 5/Zadanie Domowe Dzień 5/Program.cs
@@ -0,0 +1,17 @@
+﻿///Przygotuj program, który policzy ile jakich cyfr
+///występuje w podanej liczbie: 4566
+///
+///0=>0
+///1=>0
+///2=>0
+///3=>0
+///4=>1
+///5=>1
+///6=>2
+///7=>0
+///8=>0
+///9=>0
+
+int number = 4566;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
